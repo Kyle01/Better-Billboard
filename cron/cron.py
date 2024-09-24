eac5961e0057e4007263b5b994ff888908899dc7
kyle01 <kylemcv@gmail.com>
2024-09-23 21:31:24 -0400

    Drop in Lambda Function straight form AWS

--- cron/cron.py
+++ cron/cron.py
@@ -1,17 +1,22 @@
-import requests
 import hashlib
 import os
+import requests
 
-URL = 'http://127.0.0.1:5000/scrape' # to change to production url
-secret = 'secret' # to change to environment file
-random_bytes = os.urandom(32)
-hashed_token = hashlib.sha256(random_bytes).hexdigest()
+def lambda_handler(event, context):
+    URL = 'https://better-billboard.onrender.com/scrape'
+    secret = os.environ['secret']
+    random_bytes = os.urandom(32)
+    hashed_token = hashlib.sha256(random_bytes).hexdigest()
+    
+    hasher = hashlib.sha256()
+    hasher.update(f'{hashed_token}{secret}'.encode('utf-8'))
+    secret_hash = hasher.hexdigest()
+    
+    headers = { "token": str(hashed_token), "hash": str(secret_hash) }
+    
+    response = requests.get(URL, headers=headers)
 
-hasher = hashlib.sha256()
-hasher.update(f'{hashed_token}{secret}'.encode('utf-8'))
-secret_hash = hasher.hexdigest()
-
-headers = { "token": str(hashed_token), "hash": str(secret_hash) }
-
-r = requests.get(URL, headers=headers)
-print(r)
+    return {
+        'statusCode': response.status_code,
+        'body': response.content
+    }
